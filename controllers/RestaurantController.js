d93a73ccd7d5450ed8f85537cd2b5f2cf670f643
Daniel <danarrarr@alum.us.es>
2022-05-31 11:28:04 +0200

    lab 2

--- controllers/RestaurantController.js
+++ controllers/RestaurantController.js
@@ -1,22 +1,65 @@
 'use strict'
 const models = require('../models')
 const Restaurant = models.Restaurant
-const Product = models.Product
 
 exports.index = async function (req, res) {
-
+  try {
+    const restaurants = await Restaurant.findAll()
+    res.json(restaurants)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
-exports.indexOwner = function (req, res) {
-  res.sendStatus(404)
+exports.indexOwner = async function (req, res) {
+  try {
+    const restaurants = await Restaurant.findAll({
+      where: {
+        restaurantId: req.user.userId
+      }
+    })
+    res.json(restaurants)
+  } catch (err) {
+    res.status(404).send(err)
+  }
 }
 
 exports.create = async function (req, res) {
-
+  const newRestaurant = Restaurant.build(req.body)
+  newRestaurant.userId = 1 // authenticated user
+  if (typeof req.files?.heroImage !== 'undefined') {
+    newRestaurant.heroImage = req.files.heroImage[0].path
+  }
+  if (typeof req.files?.logo !== 'undefined') {
+    newRestaurant.logo = req.files.logo[0].path
+  }
+  try {
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    if (err.name.includes('ValidationError')) {
+      res.status(422).send(err)
+    } else {
+      res.status(500).send(err)
+    }
+  }
 }
 
 exports.show = async function (req, res) {
-
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId,
+      {
+        include: [
+          {
+            model: Restaurant,
+            as: 'restaurants'
+          }
+        ]
+      })
+    res.json(restaurant)
+  } catch (err) {
+    res.status(404).send(err)
+  }
 }
 
 // Implement below, the update and destroy functions as well
--- routes/RestaurantRoutes.js
+++ routes/RestaurantRoutes.js
@@ -1,9 +1,53 @@
 'use strict'
 const RestaurantController = require('../controllers/RestaurantController')
 const OrderController = require('../controllers/OrderController')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const fs = require('fs')
+    fs.mkdirSync(process.env.PRODUCTS_FOLDER, { recursive: true })
+    cb(null, process.env.PRODUCTS_FOLDER)
+  },
+  filename: function (req, file, cb) {
+    cb(null, Math.random().toString(36).substring(7) + '-' + Date.now() + '.' + file.originalname.split('.').pop())
+  }
+})
+const upload = multer({ storage: storage }).single('image')
 
 module.exports = (options) => {
   const app = options.app
-
+  app.route('/restaurants')
+    .get(
+      RestaurantController.index
+    )
+    .post(
+      upload,
+      RestaurantController.create
+    )
+  app.route('/restaurants/{restaurantId}')
+    .get(
+      RestaurantController.show
+    )
+    .put(
+      upload,
+      RestaurantController.update
+    )
+    .delete(
+      RestaurantController.destroy
+    )
+    .update(
+      RestaurantController.update
+    )
+  // FR 3
+  app.route('/restaurants/{restaurantId}/orders')
+    .get(
+      OrderController.indexRestaurant
+    )
+  // FR 5
+  app.route('/restaurants/:restaurantId/analytics')
+    .get(
+      OrderController.analytics
+    )
   // TODO: Include routes for restaurant described in the lab session wiki page.
 }
